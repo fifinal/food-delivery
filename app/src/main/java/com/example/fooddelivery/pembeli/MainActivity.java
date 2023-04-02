fddc4cf5946746bf274c3c5dcd6a2d439c52d834
slamet fifin alamsyah <s.fifinalamsyah@gmail.com>
2023-04-02 12:16:05 +0700

    first commit

---
+++ app/src/main/java/com/example/fooddelivery/pembeli/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.fooddelivery.pembeli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fooddelivery.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/fooddelivery/pembeli/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.fooddelivery.pembeli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fooddelivery.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
